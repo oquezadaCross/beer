de832751d8ec68dbf6ffe377ea0ca6c3cc66c64f
Oscar <oquezada@kdu.cl>
2019-12-06 23:58:10 -0300

    Controller rest search

--- src/main/java/com/cross/beer/controller/PartnerController.java
+++ src/main/java/com/cross/beer/controller/PartnerController.java
@@ -2,6 +2,7 @@ package com.cross.beer.controller;
 
 import com.cross.beer.model.Partner;
 import com.cross.beer.service.PartnerService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -33,6 +34,11 @@ public class PartnerController {
     public ResponseEntity<Partner> findById(@PathVariable("id") Long id) {
         Partner partner = new Partner();
         return new ResponseEntity<>(partner, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/{lng}/{lat}")
+    public ResponseEntity<List<Partner>> search(@PathVariable("lng") Double lng, @PathVariable("lat") Double lat) {
+        return new ResponseEntity<>(partnerService.search(lng, lat), HttpStatus.OK);
     }
 
 
--- src/main/java/com/cross/beer/model/Partner.java
+++ src/main/java/com/cross/beer/model/Partner.java
@@ -3,6 +3,7 @@ package com.cross.beer.model;
 import lombok.Data;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 /**
@@ -19,6 +20,8 @@ public class Partner {
     private String ownerName;
     private String document;
     private CoverageArea coverageArea;
+
+    @Indexed
     private Address address;
 
 }
